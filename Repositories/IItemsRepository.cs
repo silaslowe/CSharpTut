ea06138615a9d75985ba599b1369b2918f6d8d9a
silaslowe@gmail.com <silaslowe@gmail.com>
2022-02-06 21:13:07 -0600

    Added async to rest methods

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -1,15 +1,16 @@
 using Catalog.Entities;
 using System.Collections.Generic;
 using System;
+using System.Threading.Tasks;
 
 namespace Catalog.Repositories
  {   
     public interface IItemsRepository
     {
-        Item GetItem(System.Guid id);
-        IEnumerable<Item> GetItems();
-        void CreateItem(Item item);
-        void UpdateItem(Item item);
-        void DeleteItem(Guid id);
+        Task<Item> GetItemAsync(System.Guid id);
+        Task<IEnumerable<Item>> GetItemsAsync();
+        Task CreateItemAsync(Item item);
+        Task UpdateItemAsync(Item item);
+        Task DeleteItemAsync(Guid id);
     }
  }
